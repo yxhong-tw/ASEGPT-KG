b3432299e6cd5647bb919920e3a84536ebf507be
zhiao777774 <zhiaohsu@gmail.com>
2023-12-05 20:44:19 +0800

    refactor: Rewrite CHINESE_ALPACA_SYSTEM_PROMPT

--- prompts/system_prompt.py
+++ prompts/system_prompt.py
@@ -1,1 +1,1 @@
-CHINESE_ALPACA_SYSTEM_PROMPT = 'You are a helpful assistant. 你是一個樂於助人的助手。請你提供專業、有邏輯、內容真實且有價值的詳細回覆。'
+CHINESE_ALPACA_SYSTEM_PROMPT = 'You are a helpful assistant. 你是一個樂於助人的助手。請你提供專業、有邏輯、內容真實且有價值的詳細回覆。你的回答對我非常重要，請幫助我完成任務，與解答任何疑惑。'
